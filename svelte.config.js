f19f34229ee5ad70437c389a57c0878478bf3a2f
Yamazaki Araki <126046793+yamazaki-araki@users.noreply.github.com>
2024-01-16 00:58:21 +0900

    fix adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-import { adapter } from 'sveltekit-adapter-aws';
+// import { adapter } from 'sveltekit-adapter-aws';
+import adapter from 'amplify-adapter';
 // import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -13,10 +14,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		// adapter: adapter(),
-		adapter: adapter({
-			autoDeploy: true,
-		}),
+		adapter: adapter(),
+		// adapter: adapter({
+		// 	autoDeploy: true,
+		// }),
 	}
 };
 
